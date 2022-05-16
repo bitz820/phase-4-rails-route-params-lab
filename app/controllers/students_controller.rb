c83c6107fa25068da6968597d853f26e4811254b
Mark <mark.n.bacon.jr@gmail.com>
2022-05-16 12:06:24 -0500

    commit tree bce44bed7cac2e515d431d353aa23c0a5d404db1

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,24 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    # byebug
+    students = if params[:name]
+      Student.find_by(first_name: params[:name]) || Student.find_by(last_name: params[:name])
+    else
+      Student.all
+    end
     render json: students
+
+    # student = Student.find_by(last_name: params[:last_name])
+
+    # render json: student
+
+    # {first_name: PARAM, last_name: PARAM}
+  end
+
+  def show 
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
